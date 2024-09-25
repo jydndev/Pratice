b033f4a5d43d815afc869507e14be719a909825f
jydnbach <jaydenkim95@gmail.com>
2024-09-25 09:18:19 +0900

    answer

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,15 @@
-function fibonacci() {}
+function fibonacci(n) {
+  if (n <= 1) return n;
+
+  let a = BigInt(0);
+  let b = BigInt(1);
+  let temp;
+
+  for (let i = 2; i <= n; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
+}
