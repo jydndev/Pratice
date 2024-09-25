a0dc5c3941cc4e2f5fa24a734b63293289ea4b0a
jydnbach <jaydenkim95@gmail.com>
2024-09-25 16:13:43 +0900

    // fibonacci recursive //  fibonacci Iterative // fibonacci memoized

--- fibonacci.js
+++ fibonacci.js
@@ -13,3 +13,30 @@ function fibonacci(n) {
 
   return b;
 }
+
+// fibonacci recursive
+function fibonacciRecursive(n) {
+  if (n <= 1) return n;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+//  fibonacci Iterative
+function fibonacciIterative(n) {
+  if (n <= 1) return n;
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+// fibonacci memoized
+function fibonacciMemoized(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 1) return n;
+  memo[n] = fibonacciMemoized(n - 1, memo) + fibonacciMemoized(n - 1, memo);
+  return memo[n];
+}
